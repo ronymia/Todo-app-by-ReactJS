ab279b71b537fb5b774dd8d96b4ca83fd63887f0
MD Rony Mia <mdronymia.javascript@gmail.com>
2023-01-25 12:55:53 +0600

    Task list state

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
+import { useState } from "react";
 import Header from "./components/Header/Header";
 import TaskList from "./components/TaskList/TaskList";
 
 function App() {
+  const [taskList, setTaskList] = useState([]);
+
+  const HandleAddToTask = (task) => {
+    const singleTask = {
+      id: (new Date()).getTime(),
+      task: task,
+      isCompleted: false
+    };
+    // set task to task list state
+    setTaskList([...taskList, singleTask]);
+  }
+
   return (
     <>
-      <Header />
+      <Header HandleAddToTask={HandleAddToTask} />
       <TaskList />
     </>
   );
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,15 +3,31 @@ import { AiOutlinePlusCircle } from 'react-icons/ai';
 import './Header.css';
 import todoLogo from '../../assets/todoLogo.svg';
 
-const Header = () => {
+const Header = ({ HandleAddToTask }) => {
+
+    const handleSubmitTask = (event) => {
+        event.preventDefault();
+        const task = (event.target.task.value);
+
+        // pass task 
+        HandleAddToTask(task);
+
+        // reset input field
+        event.target.reset();
+    }
+
     return (
         <header className="header">
             <figure>
                 <img src={todoLogo} alt="" />
             </figure>
-            <form className="newTaskForm">
+            <form onSubmit={handleSubmitTask} className="newTaskForm">
                 <input type="Add a new task" name="task" />
-                <button type="submit">create <AiOutlinePlusCircle size={20} /></button>
+                <button type="submit"
+                >
+                    create
+                    <AiOutlinePlusCircle size={20} />
+                </button>
             </form>
         </header>
     )
